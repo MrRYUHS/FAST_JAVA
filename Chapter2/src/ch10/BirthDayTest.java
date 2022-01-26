0d12bf4688e12e48e383b6f36577cf5e1e7770a8
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-01-26 14:07:25 +0900

    자바 언어 기초

---
+++ Chapter2/src/ch10/BirthDayTest.java
@@ -0,0 +1,17 @@
+package ch10;
+
+public class BirthDayTest {
+
+	public static void main(String[] args) {
+
+		BirthDay date = new BirthDay();
+		date.setYear(2022);
+		date.setMonth(06);
+		date.setDay(23);
+		
+		date.showDate();
+		
+		
+	}
+
+}
