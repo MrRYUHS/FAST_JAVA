b6385af13e0ec8ff5445a3cdb1804ca7ea84e2ed
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-03-23 22:31:59 +0900

    practice

--- Chapter4/src/ch03/StringBuilderTest.java
+++ Chapter4/src/ch03/StringBuilderTest.java
@@ -16,5 +16,4 @@ public class StringBuilderTest {
 		String test = buffer.toString();
 		System.out.println(test);
 	}
-
 }
--- Chapter4/src/ch03/StringTest.java
+++ Chapter4/src/ch03/StringTest.java
@@ -12,5 +12,4 @@ public class StringTest {
 		
 		System.out.println(System.identityHashCode(java));
 	}
-
 }
