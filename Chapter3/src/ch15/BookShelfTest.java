300b1651be8fad41134f7aee1e434d19dc99d4b2
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-03-30 11:32:01 +0900

    practice

--- Chapter3/src/ch15/BookShelfTest.java
+++ Chapter3/src/ch15/BookShelfTest.java
@@ -5,10 +5,10 @@ public class BookShelfTest {
 	public static void main(String[] args) {
 
 		Queue bookQueue = new BookShelf();
-		bookQueue.enQueue("토지1");
+		bookQueue.enQueue("농장1");
 		bookQueue.enQueue("토지2");
-		bookQueue.enQueue("토지3");
-		bookQueue.enQueue("토지4");
+		bookQueue.enQueue("농장3");
+		bookQueue.enQueue("바다4");
 		bookQueue.enQueue("토지5");
 		
 		System.out.println(bookQueue.getSize());
