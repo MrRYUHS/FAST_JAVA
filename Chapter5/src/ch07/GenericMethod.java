91037b7fabff76f2504f4c7f5390e52df192f717
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-04-15 17:43:56 +0900

    연습

--- Chapter5/src/ch07/GenericMethod.java
+++ Chapter5/src/ch07/GenericMethod.java
@@ -20,7 +20,7 @@ public class GenericMethod {
 	public static void main(String[] args) {
 
 		Point<Integer, Double> p1 = new Point<Integer, Double>(0, 0.0);
-		Point<Integer, Double> p2 = new Point<>(10, 10.0);
+		Point<Integer, Double> p2 = new Point<>(100, 100.0);
 		
 		double size = GenericMethod.<Integer, Double>makeRectangle(p1,p2);
 		System.out.println(size);
