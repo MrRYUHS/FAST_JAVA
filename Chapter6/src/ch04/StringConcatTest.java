3e6732fc047bb600ff28d17ff6233c733066523b
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-04-20 09:44:31 +0900

    챕터6-5

---
+++ Chapter6/src/ch04/StringConcatTest.java
@@ -0,0 +1,27 @@
+package ch04;
+
+public class StringConcatTest {
+
+	public static void main(String[] args) {
+
+		String s1 = "LostArk";
+		String s2 = "RodyCandy";
+		
+		StringConcatImpl strImpl = new StringConcatImpl();
+		strImpl.makeString("LostArk", "RodyCandy");
+		
+		StringConcat concat = (s, v)->System.out.println(s+ "," +v);
+		concat.makeString(s1, s2);
+		
+		StringConcat concat2 = new StringConcat() {
+
+			@Override
+			public void makeString(String s1, String s2) {
+
+				System.out.println(s1 + "...." +s2);
+			}			
+		};
+		
+		concat2.makeString(s1,s2);
+	}
+}
