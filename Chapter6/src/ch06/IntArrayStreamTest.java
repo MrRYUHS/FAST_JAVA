7a228f53d5eb4528bf89b32c20a259a845336051
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-04-24 23:39:03 +0900

    practice

--- Chapter6/src/ch06/IntArrayStreamTest.java
+++ Chapter6/src/ch06/IntArrayStreamTest.java
@@ -7,7 +7,7 @@ public class IntArrayStreamTest {
 
 	public static void main(String[] args) {
 
-		int[] arr = {1,2,3,4,5};
+		int[] arr = {1,2,3,4,5,6,7,8,9,10};
 		
 		for(int num :arr) {
 			System.out.println(num);
